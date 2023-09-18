e83acbe101ad500c8c6a2e76f4c58d163cf84670
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-09-18 10:36:00 -0400

    main

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*After completing the implementation, you should create the schema from Assignment 1 using
+your own 'mydb' implementation and insert sample data into the schema. Then, implement the
+queries from Assignment 1 using your own relational algebra operators implementation and
+print the resulting tables. Additionally, for each operator you implemented, provide one
+meaningful query implementation using your 'mydb' implementation and print the tables. Do this
+task in a class with the name Main under the default package */
+public class Main {
+
+  public static void main(String args[]) {}
+}
