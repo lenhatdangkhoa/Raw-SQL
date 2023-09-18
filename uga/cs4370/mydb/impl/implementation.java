235612cff261ef12efb5cdef399f45d2b2742e9f
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-09-18 10:00:43 -0400

    implementation.java

---
+++ uga/cs4370/mydb/impl/implementation.java
@@ -0,0 +1,12 @@
+package uga.cs4370.mydb.impl;
+
+/**
+ * You must implement the provided interfaces in this project. You should not modify the provided
+interfaces. However, you can include additional methods in the implementing classes as
+needed.
+
+Your implementation must use the interfaces as types whenever it is possible instead of
+using concrete implementing classes as types. You can not use any external libraries in this
+project. You can use Java built in data structures.
+ */
+public class implementation {}
