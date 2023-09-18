2a075c8b0117b398560938c38bf64182ed537c21
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-09-18 10:08:32 -0400

    setup implementation boilerplate code

--- uga/cs4370/mydb/impl/implementation.java
+++ uga/cs4370/mydb/impl/implementation.java
@@ -1,5 +1,15 @@
 package uga.cs4370.mydb.impl;
 
+/* public class */
+import uga.cs4370.mydb.Cell;
+/* Interfaces */
+import uga.cs4370.mydb.Predicate;
+import uga.cs4370.mydb.RA;
+import uga.cs4370.mydb.Relation;
+import uga.cs4370.mydb.RelationBuilder;
+/* public enum */
+import uga.cs4370.mydb.Type;
+
 /**
  * You must implement the provided interfaces in this project. You should not modify the provided
 interfaces. However, you can include additional methods in the implementing classes as
@@ -9,4 +19,5 @@ Your implementation must use the interfaces as types whenever it is possible ins
 using concrete implementing classes as types. You can not use any external libraries in this
 project. You can use Java built in data structures.
  */
-public class implementation {}
+public class implementation
+  implements Predicate, RA, Relation, RelationBuilder {}
