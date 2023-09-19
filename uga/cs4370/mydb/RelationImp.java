3ffdc01a5c42916259d7938f9c903ccdc61b23a4
Khoa Le <kl58277@uga.edu>
2023-09-19 16:46:40 -0400

    added relationimp

---
+++ uga/cs4370/mydb/RelationImp.java
@@ -0,0 +1,23 @@
+package uga.cs4370.mydb;
+
+import java.util.List;
+
+public class RelationImp implements Relation {
+
+    String name;
+    int size;
+    List<List<Cell>> rows;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public List<List<Cell>> getRows() {
+
+    }
+
+}
