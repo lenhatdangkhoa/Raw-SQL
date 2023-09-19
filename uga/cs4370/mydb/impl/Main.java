b03ec5823a8133e68a4ce66d85b57036ef113766
Khoa Le <kl58277@uga.edu>
2023-09-19 10:30:40 -0400

    restructured directory

--- uga/cs4370/mydb/impl/Main.java
+++ uga/cs4370/mydb/impl/Main.java
@@ -1,3 +1,5 @@
+package uga.cs4370.mydb.impl;
+
 /*After completing the implementation, you should create the schema from Assignment 1 using
 your own 'mydb' implementation and insert sample data into the schema. Then, implement the
 queries from Assignment 1 using your own relational algebra operators implementation and
@@ -6,5 +8,6 @@ meaningful query implementation using your 'mydb' implementation and print the t
 task in a class with the name Main under the default package */
 public class Main {
 
-  public static void main(String args[]) {}
+  public static void main(String args[]) {
+  }
 }
--- uga/cs4370/mydb/impl/implementation.java
+++ uga/cs4370/mydb/impl/implementation.java
@@ -12,36 +12,61 @@ import uga.cs4370.mydb.RelationBuilder;
 import uga.cs4370.mydb.Type;
 
 /**
- * You must implement the provided interfaces in this project. You should not modify the provided
-interfaces. However, you can include additional methods in the implementing classes as
-needed.
-
-Your implementation must use the interfaces as types whenever it is possible instead of
-using concrete implementing classes as types. You can not use any external libraries in this
-project. You can use Java built in data structures.
+ * You must implement the provided interfaces in this project. You should not
+ * modify the provided
+ * interfaces. However, you can include additional methods in the implementing
+ * classes as
+ * needed.
+ * 
+ * Your implementation must use the interfaces as types whenever it is possible
+ * instead of
+ * using concrete implementing classes as types. You can not use any external
+ * libraries in this
+ * project. You can use Java built in data structures.
  */
-public class implementation
-  implements Predicate, RA, Relation, RelationBuilder {
+/*
+public class Implementation
+    implements Predicate, RA, Relation, RelationBuilder {
 
-  public String getName() {}
+  public String getName() {
+    return "hello";
+  }
 
-  public int getSize() {}
+  public int getSize() {
+    return 0;
+  }
 
-  public List<List<Cell>> getRows() {}
+  public List<List<Cell>> getRows() {
+    return null;
+  }
 
-  public List<Type> getTypes() {}
+  public List<Type> getTypes() {
+    return null;
+  }
 
-  public List<String> getAttrs() {}
+  public List<String> getAttrs() {
+    return null;
+  }
 
-  public boolean hasAttr(String attr) {}
+  public boolean hasAttr(String attr) {
+    return false;
+  }
 
-  public int getAttrIndex(String attr) {}
+  public int getAttrIndex(String attr) {
+    return 0;
+  }
 
-  public void insert(Cell... cells) {}
+  public void insert(Cell... cells) {
+  }
 
-  public void insert(List<Cell> cells) {}
+  public void insert(List<Cell> cells) {
+  }
 
-  public void print() {}
+  public void print() {
+  }
 
-  public boolean check(List<Cell> row) {}
+  public boolean check(List<Cell> row) {
+    return false;
+  }
 }
+*/
