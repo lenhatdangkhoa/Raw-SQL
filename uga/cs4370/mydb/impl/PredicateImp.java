222e7c2b700a75680fa136a7a38986627fe23b57
ragegoney2 <gagemroney@gmail.com>
2023-09-25 17:57:46 -0400

    working on predicate

---
+++ uga/cs4370/mydb/impl/PredicateImp.java
@@ -0,0 +1,17 @@
+package uga.cs4370.mydb.impl;
+
+import uga.cs4370.mydb.Cell;
+import java.util.List;
+import uga.cs4370.mydb.Predicate;
+
+public class PredicateImp implements Predicate {
+    
+    private boolean predStatement = false;
+
+    public boolean check (List<Cell> row) {
+        
+     
+        return predStatement;
+    }
+
+}
